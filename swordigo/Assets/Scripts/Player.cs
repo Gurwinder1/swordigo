b3063d93543969809065d113a8a262442e2be18e
Gurwinder <33232439+Gurwinder1@users.noreply.github.com>
2017-11-07 18:47:21 -0500

    build 2
    1

---
+++ swordigo/Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private Rigidbody2D myRigidbody;
+
+    [SerializeField]
+    private float movementSpeed;
+
+    private bool facingRight;
+
+	// Use this for initialization
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate()
+    { 
+
+        float horizontal = Input.GetAxis("Horizontal");
+       
+
+        HandleMovement(horizontal);
+
+        Flip(horizontal);
+		
+	}
+    private void HandleMovement(float horizontal)
+    {
+        myRigidbody.velocity = new Vector2(horizontal * movementSpeed, myRigidbody.velocity.y);
+    }
+
+    private void Flip(float horizontal)
+    {
+        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+
+            transform.localScale = theScale;
+           
+
+        }
+
+    }
+
+}
